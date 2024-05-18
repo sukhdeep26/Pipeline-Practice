32ae11cc427092499f0b55f1d311b9df539ffa3b
root <root@ip-172-31-34-174.us-east-2.compute.internal>
2024-05-18 23:39:55 +0000

    new

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, DevSecOps! This is Sonam.'
+    return 'Hello, DevSecOps!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
